1e6e6fd5daf1dd8a9a49d9275b75f959338586bf
nnhao14102000 <nnhao9a3@gmail.com>
2021-01-06 16:32:14 +0700

    Add project files.

---
+++ StudentManagement/Controllers/StudentController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentManagement.Models;
+
+namespace StudentManagement.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Students.ToList());
+            }
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            using (DBModels db = new DBModels())        
+            {
+                return View(db.Students.Where(x => x.Id == id).FirstOrDefault());
+            }
+            
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (DBModels db = new DBModels())
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Students.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Student student)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (DBModels db = new DBModels())
+                {
+                    db.Entry(student).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Students.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Student student)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using(DBModels db = new DBModels())
+                {
+                    student = db.Students.Where(x => x.Id == id).FirstOrDefault();
+                    db.Students.Remove(student);
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
